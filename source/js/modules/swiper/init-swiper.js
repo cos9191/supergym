647687fa05afabbe8341c7c0ba98bcaf085ad0c6
Konstantin Fedorov <cos9191@mail.ru>
2023-05-22 20:03:26 +0700

    + wip pages into sitemap.html

--- source/js/modules/swiper/init-swiper.js
+++ source/js/modules/swiper/init-swiper.js
@@ -1,12 +1,13 @@
 // import Swiper bundle with all modules installed
 import Swiper from '../../vendor/swiper-bundle';
+// import Swiper from '../../vendor/swiper-bundle';
 
 const staffSwiperNode = document.querySelector('.staff__swiper');
 const reviewsSwiperNode = document.querySelector('.reviews__swiper');
 
 export const initSwiper = () => {
   if (staffSwiperNode) {
-    const staffSwiper = new Swiper('.staff__swiper', {
+    const staffSwiper = new Swiper('.staff__swiper', { // eslint-disable-line
 
       // Optional parameters
       direction: 'horizontal',
@@ -36,7 +37,7 @@ export const initSwiper = () => {
   }
 
   if (reviewsSwiperNode) {
-    const reviewsSwiper = new Swiper('.reviews__swiper', {
+    const reviewsSwiper = new Swiper('.reviews__swiper', { // eslint-disable-line
 
       // Optional parameters
       direction: 'horizontal',
@@ -53,51 +54,3 @@ export const initSwiper = () => {
     });
   }
 };
-
-// if (staffSwiperNode) {
-//   (() => new Swiper('.staff__swiper', {
-//
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
-//     slidesPerView: 1,
-//     spaceBetween: 2,
-//
-//     breakpoints: {
-//       // when window width is >= 768px
-//       768: {
-//         slidesPerView: 2,
-//         spaceBetween: 30,
-//       },
-//       // when window width is >= 1200px
-//       1200: {
-//         slidesPerView: 4,
-//         spaceBetween: 40,
-//       },
-//     },
-//
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   }))();
-// }
-//
-// if (reviewsSwiperNode) {
-//   (() => new Swiper('.reviews__swiper', {
-//
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: false,
-//     autoHeight: true,
-//     slidesPerView: 1,
-//     spaceBetween: 2,
-//
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   }))();
-// }
